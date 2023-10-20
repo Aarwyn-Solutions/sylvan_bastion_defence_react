cbf4c2c8944f8965653a9fad1590d3670b2836a6
NoSpooksAllowed <hersgori4@protonmail.com>
2023-10-20 19:51:18 -0300

    initialized state for bastionInfo and DungeonInfo and connect Bastion|Dungeon component event with Menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useState } from "react";
 
 import Map from "./components/Map";
+import Menu from "./components/Menu";
+import { DungeonInfo } from "./components/Dungeon";
+import { BastionInfo } from "./components/Bastion";
 
 const App: React.FC = () => {
   const mapImageUrl = "./src/assets/game/map.jpg";
+  const [selectedBlock, setSelectedBlock] = useState<BastionInfo | DungeonInfo | null>(null);
+
+  const appStyles: React.CSSProperties = {
+    display: "flex",
+    flexWrap: "wrap",
+  };
+
+  const handleBlockClick = (info: BastionInfo | DungeonInfo) => {
+    setSelectedBlock(info);
+  };
+
   return (
     <>
       <h1>Hello Map</h1>
-      <Map imageUrl={mapImageUrl} />
+      <div className="container" style={appStyles}>
+        <Map imageUrl={mapImageUrl} onBlockClick={handleBlockClick} />
+        <Menu selectedBlock={selectedBlock} />
+      </div>
     </>
   );
 };
